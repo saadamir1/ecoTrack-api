216d1aa7b83f1815a33a6af073770c6d1bc97610
saadamir1 <saadamir070@gmail.com>
2025-04-24 01:40:38 +0500

    routes for activity related endpoints

--- routes/challengeParticipationRoutes.js
+++ routes/challengeParticipationRoutes.js
@@ -1,18 +1,39 @@
 const express = require('express');
 const router = express.Router();
-
 const {
   joinChallenge,
   leaveChallenge,
   updateProgress,
   getUserChallenges,
-  getChallengeParticipants
+  getChallengeParticipants,
+  getParticipation,
+  addActivity,
+  getActivities,
+  deleteActivity
 } = require('../controllers/ChallengeParticipationController');
 
-router.post('/join', joinChallenge);
-router.put('/leave/:challengeId/:userId', leaveChallenge);
-router.put('/progress/:challengeId/:userId', updateProgress);
+// Get user challenges
 router.get('/user/:userId', getUserChallenges);
-router.get('/:challengeId/participants', getChallengeParticipants);
+
+// Join a challenge
+router.post('/join', joinChallenge);
+
+// Leave a challenge
+router.put('/leave/:challengeId/:userId', leaveChallenge);
+
+
+// Update challenge progress
+router.put('/:challengeId/user/:userId/progress', updateProgress);
+
+// Get challenge participants
+router.get('/challenge/:challengeId/participants', getChallengeParticipants);
+
+// Get a single participation
+router.get('/:participationId', getParticipation);
+
+// Activity management
+router.post('/:participationId/activities', addActivity);
+router.get('/:participationId/activities', getActivities);
+router.delete('/:participationId/activities/:activityId', deleteActivity);
 
 module.exports = router;
