13c01e017c2318f87393e26a5c785d3fab81423a
saadamir1 <saadamir070@gmail.com>
2025-04-25 02:07:33 +0500

    fix login and signup methods to populate profiles in user objects

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,6 @@ const organizationRoutes = require('./organizationRoutes');
 const categoryRoutes = require('./categoryRoutes');
 const adminRoutes = require('./adminRoutes');
 const challengeParticipationRoutes = require('./challengeParticipationRoutes');
-const chatbotRoutes = require('./chatbotroutes');
 
 const router = express.Router();
 
@@ -15,6 +14,5 @@ router.use('/organizations', organizationRoutes);
 router.use('/categories', categoryRoutes);
 router.use('/admin', adminRoutes);
 router.use('/challengeParticipations', challengeParticipationRoutes);
-router.use('/chat', chatbotRoutes);
 
 module.exports = router;
